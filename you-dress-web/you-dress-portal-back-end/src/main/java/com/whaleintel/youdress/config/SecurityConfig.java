be5bf0e4f7a64db70583627f32cc2904dade5228
mapie-picking <mapie-pick-up@proton.me>
2025-08-30 20:54:31 +0300

    update-30-08-2025 #2

--- you-dress-web/you-dress-portal-back-end/src/main/java/com/whaleintel/youdress/config/SecurityConfig.java
+++ you-dress-web/you-dress-portal-back-end/src/main/java/com/whaleintel/youdress/config/SecurityConfig.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.whaleintel.youdress.servicegateway.config;
+package com.whaleintel.youdress.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
